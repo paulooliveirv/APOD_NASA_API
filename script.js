5dc6b2e50b7837901fa42a985e4ed5a8d0212fca
Paulo Reis <paauloreis@yahoo.com.br>
2022-02-01 03:12:09 -0300

    avancando com css

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
+const input = $("#botao");
+input.on('click', ()=>{
+    apod();
+});
 
 
-const pegar = fetch('https://api.nasa.gov/planetary/apod?api_key=AKK3qowH4jjLe1gDU306EOd1n7lDohpJ3hGRGBAY',{
+function apod(){
+    const data = $('#data').val();
+    const titulo = $('#title');
+    const img = $('#img');
+    const video = $('#video');
+    const copy = $('#copy');
+    const cont = $('#conteudo');
+    
+    $.ajax({
+        url:`https://api.nasa.gov/planetary/apod?api_key=qEUarBdDDAztcHdPDePgdLm6NZeIjVdutrrvJPfL&date=${data}`,
+    success: function(recebido){
 
-method: 'GET'
+        img.attr("src", recebido.url);
+        titulo.text(recebido.title);
+        cont.text(recebido.explanation);
+        copy.text(recebido.copy);
+
+        if (recebido.media_type === 'image'){
+            img.attr("src", recebido.url);
+            img.css('display', 'block');
+            video.css('display', 'none');
+            cont.css("display", "block");
+            copy.css("display", "block");
+        }else{
+            video.attr("src", recebido.url);
+            video.css('display', 'block');
+            img.css('display', 'none');
+            cont.css("display", "block");
+            copy.css("display", "block");
+        }
+    },
+    error: ()=>{
+        console.log('deu ruim');
+    }
 })
-.then(function(respostaDoServidor){
-console.log(respostaDoServidor)
-})
+}
